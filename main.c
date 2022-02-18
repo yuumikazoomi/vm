11cba08eaa220eac546744e97b190f92b055664b
yuumikazoomi <50451019+yuumikazoomi@users.noreply.github.com>
2022-02-18 15:46:42 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include "vm.h"
 
-
-uint8_t addtwodigits[] = {
-
+uint8_t bytecode[] = {
     kMovConst,  kR0,    10,
     kAddConst,  kR0,    5,
     kMovConst,  kR1,    3,
@@ -12,11 +10,14 @@ uint8_t addtwodigits[] = {
     kMovReg,    kR1,    kR0,
     kSubConst,  kR1,    7,
     kMovReg,    kRax,   kR1,
+    kCall,      0x00,   0x00,
     kHalt
 };
+
 int main(int argc, const char * argv[]) {
+    
     struct vm_context* ctx = initvm();
-    ctx->instructions = addtwodigits;
+    ctx->instructions = bytecode;
     vm(ctx);
     printf("%llu\n",ctx->registers.rax);
     destroyvm(ctx);
