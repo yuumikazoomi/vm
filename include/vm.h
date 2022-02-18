c8455392ed10bfab14924e315cf53b97ee85c042
yuumikazoomi <50451019+yuumikazoomi@users.noreply.github.com>
2022-02-18 10:28:40 -0500

    changes to registers

--- include/vm.h
+++ include/vm.h
@@ -1,10 +1,3 @@
-//
-//  vm.h
-//  vm
-//
-//  Created by Irelia on 2/17/22.
-//
-
 #ifndef vm_h
 #define vm_h
 
@@ -15,8 +8,9 @@
 enum vm_op : uint8_t{
     kR0,
     kR1,
+    kR2,
+    kR3,
     kRax,
-    kRdx,
     kHalt,
     kMovReg,
     kMovConst,
@@ -25,12 +19,27 @@ enum vm_op : uint8_t{
     kSubReg,
     kSubConst,
 };
+/*
+ *Calling convention - TO BE ADDED LATER
+ *Four registers, r0,r1,r2,r3
+ *Four available arguments all passed through r0-r3 in respective order
+ *r0,r1,r2,r3,rax are volatile registers , do not need to be preserved, callee can modfiy
+ *rsp is non volatile and needs to be preserved
+ *no arguments are passed on the stack
+ *return address is stored on the stack
+ *stack size is 256 bytes allowing for 32 'stack-frames'
+ *return value is in rax
+ *call instruction is 9 bytes
+ *call address
+ */
 struct vm_reg{
     int64_t r0;
     int64_t r1;
+    int64_t r2;
+    int64_t r3;
     int64_t rax;
-    int64_t rdx;
     int64_t rip;
+    
 };
 struct vm_context{
     struct vm_reg registers;
